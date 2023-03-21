66cccec4d86aa1a9a494a4ad8be75f4b0e00d8c6
Julia.Matviishyna <julia.matviishyna@simanalytics.com>
2023-03-21 16:32:01 +0200

    initial commit

---
+++ stories/demo-story.stories.js
@@ -0,0 +1,25 @@
+import { html } from 'lit';
+
+
+export default {
+  title: 'DemoStory',
+  component: 'demo-story-my',
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+function Template({ header, backgroundColor }) {
+  return html`
+    <div
+      style="--demo-story-background-color: ${backgroundColor || 'white'}"
+    >
+      Hello World
+    </div>
+  `;
+}
+
+export const App = Template.bind({});
+App.args = {
+  header: 'My app',
+};
